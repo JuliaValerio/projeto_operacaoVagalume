7a9b1a79b8cc356d90382412755262c8c4780c57
Julia Valerio <juliav286@gmail.com>
2019-07-09 19:14:27 -0300

    start project operação vagalume

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    arrowKeys: true,
+    controlsContainer: '.arrow__navegation--banner',
+    nav: false
+  });
+
