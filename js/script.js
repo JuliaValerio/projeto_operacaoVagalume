60cf608178b273887a3fb852b18c47a8bfd4f513
Julia Valerio <juliav286@gmail.com>
2019-07-10 16:25:08 -0300

    Novas atualizações

--- js/script.js
+++ js/script.js
@@ -8,3 +8,28 @@ let slider = tns({
     nav: false
   });
 
+  var secondSlider = tns({
+    container: '.my-secondSlider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    arrowKeys: true,
+    controlsContainer: '.secondArrow',
+    responsive: {
+      420: {
+        items: 1,   
+      },
+  
+      1024: {
+        items: 2,
+      },
+  
+      768: {
+        items: 2,
+      },
+  
+      1280: {
+        items: 3,
+      }
+    }
+  });
